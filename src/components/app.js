9a5e3eeddeace39b1fbe67378472c6b8ac88f040
Paavai13 <paavaisen13@gmail.com>
2020-06-26 09:45:45 +0530

    26-07-2020

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+import {Header,Footer} from './layouts/index'
+import Exercise from './exercises/index'
+
+export default class extends Component {
+  render() {
+    return  <Fragment >
+  <Header />
+  <Footer />
+
+  </Fragment>
+  }
+}
