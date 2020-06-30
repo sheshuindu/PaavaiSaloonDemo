cbaf64a3230b0a7a5fa69f8d517784066f390530
Paavai13 <paavaisen13@gmail.com>
2020-06-30 18:57:57 +0530

    services

---
+++ js/services.js
@@ -0,0 +1,386 @@
+document.getElementById('AdvStyle').onclick = function (element) {
+    element.style= 'color : yellow'
+    const loader = `<table id="menservice" style="background-color: black; color: pink">
+    <tr>
+      <th>ADVANCED STYLING</th>
+      <th>Member</th>
+      <th>Non-Member</th>
+    </tr>
+    <tr>
+      <td>Ironing</td>
+      <td>458</td>
+      <td>466</td>
+    </tr>
+    <tr>
+      <td>STRAIGHTENING/SMOOTHENING - SHORT</td>
+      <td>522</td>
+      <td>766</td>
+    </tr>
+    <tr>
+      <td>STRAIGHTENING/SMOOTHENING - MEDIUM</td>
+      <td>200</td>
+      <td>100</td>
+    </tr>
+    <tr>
+      <td>Rebonding- Short</td>
+      <td>300</td>
+      <td>500</td>
+    </tr>
+    <tr>
+      <td>Rebonding- Medium</td>
+      <td>400</td>
+      <td>500</td>
+    </tr>
+   
+  </table>
+  `
+
+  clearLoader();
+  
+  document.getElementById('row').insertAdjacentHTML('afterend', loader);
+}
+
+document.getElementById('makeup').onclick = function (element) {
+  element.style= 'color : yellow'
+  const loader = `<table id="menservice" style="background-color: black; color: pink">
+  <tr>
+    <th>MAKEUP & STYLING</th>
+    <th>Member</th>
+    <th>Non-Member</th>
+  </tr>
+  <tr>
+    <td>Trendy Kiddy Cut</td>
+    <td>458</td>
+    <td>466</td>
+  </tr>
+  <tr>
+    <td>HAIR CUT-BASIC(with shampoo,conditioner and blast dry)</td>
+    <td>522</td>
+    <td>766</td>
+  </tr>
+  <tr>
+    <td>HAIR CUT-ADVANCE(with shampoo,conditioner and blast dry)</td>
+    <td>200</td>
+    <td>100</td>
+  </tr>
+  <tr>
+    <td>Shave</td>
+    <td>300</td>
+    <td>500</td>
+  </tr>
+  <tr>
+    <td>SHAMPOO-SHORT/MEDIUM</td>
+    <td>400</td>
+    <td>500</td>
+  </tr>
+ 
+</table>
+`
+
+clearLoader();
+
+document.getElementById('row').insertAdjacentHTML('afterend', loader);
+}
+
+document.getElementById('detan').onclick = function (element) {
+  element.style= 'color : yellow'
+  const loader = `<table id="menservice" style="background-color: black; color: pink">
+  <tr>
+    <th>DETAN & BLEACH</th>
+    <th>Member</th>
+    <th>Non-Member</th>
+  </tr>
+  <tr>
+    <td>Face/Neck(CREAM BLEACH)</td>
+    <td>458</td>
+    <td>466</td>
+  </tr>
+  <tr>
+    <td>Feet(CREAM BLEACH)</td>
+    <td>522</td>
+    <td>766</td>
+  </tr>
+  <tr>
+    <td>Face/Neck(DETAN)</td>
+    <td>200</td>
+    <td>100</td>
+  </tr>
+  <tr>
+    <td>Feet(DETAN)</td>
+    <td>300</td>
+    <td>500</td>
+  </tr>
+  <tr>
+    <td>Full Body(DETAN)</td>
+    <td>1400</td>
+    <td>1500</td>
+  </tr>
+ 
+</table>
+`
+
+clearLoader();
+
+document.getElementById('row').insertAdjacentHTML('afterend', loader);
+}
+
+document.getElementById('facial').onclick = function (element) {
+  element.style= 'color : yellow'
+  const loader = `<table id="menservice" style="background-color: black; color: pink">
+  <tr>
+    <th>FACIAL IS EVERYTHING</th>
+    <th>Member</th>
+    <th>Non-Member</th>
+  </tr>
+  <tr>
+    <td>Face Cleanup</td>
+    <td>458</td>
+    <td>466</td>
+  </tr>
+  <tr>
+    <td>Express Glow Facial</td>
+    <td>522</td>
+    <td>766</td>
+  </tr>
+  <tr>
+    <td>Facial</td>
+    <td>200</td>
+    <td>100</td>
+  </tr>
+  <tr>
+    <td>Ultimate Facial</td>
+    <td>300</td>
+    <td>500</td>
+  </tr>
+  <tr>
+    <td>Add-On Special Mask</td>
+    <td>400</td>
+    <td>500</td>
+  </tr>
+ 
+</table>
+`
+
+clearLoader();
+
+document.getElementById('row').insertAdjacentHTML('afterend', loader);
+}
+
+document.getElementById('hair').onclick = function (element) {
+  element.style= 'color : yellow'
+  const loader = `<table id="menservice" style="background-color: black; color: pink">
+  <tr>
+    <th>HAIR COLOURING</th>
+    <th>Member</th>
+    <th>Non-Member</th>
+  </tr>
+  <tr>
+    <td>Pro10 Colouring</td>
+    <td>458</td>
+    <td>466</td>
+  </tr>
+  <tr>
+    <td>Global Hair Colouring</td>
+    <td>522</td>
+    <td>766</td>
+  </tr>
+  <tr>
+    <td>Premium Hair Colouring</td>
+    <td>200</td>
+    <td>100</td>
+  </tr>
+  <tr>
+    <td>Henna Colouring</td>
+    <td>300</td>
+    <td>500</td>
+  </tr>
+  <tr>
+    <td>Beird/Moustache Colouring</td>
+    <td>400</td>
+    <td>500</td>
+  </tr>
+ 
+</table>
+`
+
+clearLoader();
+
+document.getElementById('row').insertAdjacentHTML('afterend', loader);
+}
+
+
+document.getElementById('AdvStyle1').onclick = function (element) {
+  element.style= 'color : yellow'
+  const loader = `<table id="menservice" style="background-color: black; color: pink">
+  <tr>
+    <th>MAKEUP AND STYLE</th>
+    <th>Member</th>
+    <th>Non-Member</th>
+  </tr>
+  <tr>
+    <td>MAKEUP(MAC)-SENIOR</td>
+    <td>3458</td>
+    <td>4166</td>
+  </tr>
+  <tr>
+    <td>Face</td>
+    <td>522</td>
+    <td>766</td>
+  </tr>
+  <tr>
+    <td>Streaks</td>
+    <td>200</td>
+    <td>100</td>
+  </tr>
+  <tr>
+    <td>Advanced Streaks</td>
+    <td>300</td>
+    <td>500</td>
+  </tr>
+  <tr>
+    <td>Platinum Glow Facial</td>
+    <td>400</td>
+    <td>500</td>
+  </tr>
+ 
+</table>
+`
+
+clearLoader();
+
+document.getElementById('row2').insertAdjacentHTML('afterend', loader);
+}
+
+document.getElementById('makeup1').onclick = function (element) {
+  element.style= 'color : yellow'
+  const loader = `<table id="menservice" style="background-color: black; color: pink">
+  <tr>
+    <th>BEST MANICURE</th>
+    <th>Member</th>
+    <th>Non-Member</th>
+  </tr>
+  <tr>
+    <td>Regular Manicure</td>
+    <td>458</td>
+    <td>466</td>
+  </tr>
+  <tr>
+    <td>Paraffin Manicure</td>
+    <td>522</td>
+    <td>766</td>
+  </tr>
+  <tr>
+    <td>Mint-O-Kool Manicure</td>
+    <td>200</td>
+    <td>100</td>
+  </tr>
+  <tr>
+    <td>Ice Cream Manicure</td>
+    <td>300</td>
+    <td>500</td>
+  </tr>
+  <tr>
+    <td>Ice Cream Manicure-Signature</td>
+    <td>400</td>
+    <td>500</td>
+  </tr>
+ 
+</table>
+`
+
+clearLoader();
+
+document.getElementById('row2').insertAdjacentHTML('afterend', loader);
+}
+
+document.getElementById('detan1').onclick = function (element) {
+  element.style= 'color : yellow'
+  const loader = `<table id="menservice" style="background-color: black; color: pink">
+  <tr>
+    <th>PEDICURE</th>
+    <th>Member</th>
+    <th>Non-Member</th>
+  </tr>
+  <tr>
+    <td>Regular Pedicure</td>
+    <td>458</td>
+    <td>466</td>
+  </tr>
+  <tr>
+    <td>Paraffin Pedicure</td>
+    <td>522</td>
+    <td>766</td>
+  </tr>
+  <tr>
+    <td>Mint-O-Kool Pedicure</td>
+    <td>200</td>
+    <td>100</td>
+  </tr>
+  <tr>
+    <td>Chocolate Foot Spa</td>
+    <td>300</td>
+    <td>500</td>
+  </tr>
+  <tr>
+    <td>Crystal Crush Pedicure</td>
+    <td>400</td>
+    <td>500</td>
+  </tr>
+ 
+</table>
+`
+
+clearLoader();
+
+document.getElementById('row2').insertAdjacentHTML('afterend', loader);
+}
+
+document.getElementById('facial1').onclick = function (element) {
+  const loader = `<table id="menservice" style="background-color: black; color: pink">
+  <tr>
+    <th>SUPER HAIR CARE</th>
+    <th>Member</th>
+    <th>Non-Member</th>
+  </tr>
+  <tr>
+    <td>Pure Coconut Nourisher</td>
+    <td>458</td>
+    <td>466</td>
+  </tr>
+  <tr>
+    <td>Menthol Chiller</td>
+    <td>522</td>
+    <td>766</td>
+  </tr>
+  <tr>
+    <td>Olive Bliss</td>
+    <td>200</td>
+    <td>100</td>
+  </tr>
+  <tr>
+    <td>Almond Indulgence</td>
+    <td>300</td>
+    <td>500</td>
+  </tr>
+  <tr>
+    <td>Hair Spa</td>
+    <td>400</td>
+    <td>500</td>
+  </tr>
+ 
+</table>
+`
+
+clearLoader();
+
+document.getElementById('row2').insertAdjacentHTML('afterend', loader);
+}
+
+
+
+const clearLoader = () => {
+  const loader = document.getElementById('menservice');
+  if(loader){
+      loader.parentElement.removeChild(loader)
+  }
+}
